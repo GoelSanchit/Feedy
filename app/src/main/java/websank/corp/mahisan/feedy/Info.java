b29fbf62086c694ccdeb0edec4c01df89592d766
voidsanchit <sanchit.goel1994@gmail.com>
2015-03-22 14:17:50 +0530

    Initial commit

---
+++ app/src/main/java/websank/corp/mahisan/feedy/Info.java
@@ -0,0 +1,99 @@
+package websank.corp.mahisan.feedy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by user on 22-03-2015.
+ */
+public class Info extends Activity {
+    ArrayList<String> MyArrList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feeds);
+        /*Intent intent = getIntent();
+        String lin = null;
+        String num = null;
+        String ema = null;
+        String add = null;
+        if (null != intent) {
+            lin = intent.getStringExtra("lin");
+            num = intent.getStringExtra("num");
+            ema = intent.getStringExtra("ema");
+            add = intent.getStringExtra("add");
+
+
+        }*/
+
+        final ListView list = (ListView) findViewById(R.id.list);
+        MyArrList = new ArrayList<String>();
+        MyArrList.add("Address");
+        MyArrList.add("Phone number");
+        MyArrList.add("Email");
+        MyArrList.add("Link");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Info.this,android.R.layout.simple_list_item_1,MyArrList);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> myAdapter, View myView,
+
+                                    int position, long mylng) {
+
+
+                switch( position )
+                {
+                    case 0:  Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                            Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34"));
+                            startActivity(intent);
+                        break;
+                    case 1:
+                        String posted_by = "+918938903941";
+
+                        String uri = "tel:" + posted_by.trim() ;
+                        intent = new Intent(Intent.ACTION_CALL);
+                        intent.setData(Uri.parse(uri));
+                        startActivity(intent);
+
+                        break;
+                    case 2:  Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                        emailIntent.setData(Uri.parse("mailto:"));
+                        emailIntent.setType("text/plain");
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+                        try {
+                            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                            finish();
+                            Log.i("Finished sending email...", "");
+                        } catch (android.content.ActivityNotFoundException ex) {
+                            Toast.makeText(Info.this,
+                                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 3:String url = "https://data.gov.in/catalog/cases-registered-under-cyber-crimes-motives";
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        break;
+
+                }
+
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/websank/corp/mahisan/feedy/MyActivity.java
@@ -0,0 +1,170 @@
+package websank.corp.mahisan.feedy;
+
+
+import java.util.ArrayList;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MyActivity extends Activity {
+
+    MyCustomAdapter dataAdapter = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        getActionBar().setTitle("Select Categories");
+        //Generate list View from ArrayList
+        displayListView();
+
+    }
+
+    private void displayListView() {
+
+        //Array list of countries
+        ArrayList<Country> countryList = new ArrayList<Country>();
+        Country country = new Country("CAGI","Comptroller And Auditor General Of India",false);
+        countryList.add(country);
+        country = new Country("DAE","Department Of Atomic Energy",false);
+        countryList.add(country);
+        country = new Country("DS","Department Of Space",false);
+        countryList.add(country);
+        country = new Country("LS","LokSabha Secretariat",false);
+        countryList.add(country);
+        country = new Country("MA","Ministry Of Agriculture",false);
+        countryList.add(country);
+        country = new Country("MCD","Ministry Of Civil Defense",false);
+        countryList.add(country);
+        country = new Country("MT","Ministry Of Tourism",false);
+        countryList.add(country);
+        country = new Country("MOA","Ministry Of Aviation",false);
+        countryList.add(country);
+
+        //create an ArrayAdaptar from the String Array
+        dataAdapter = new MyCustomAdapter(this,
+                R.layout.list_item, countryList);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        // Assign adapter to ListView
+        listView.setAdapter(dataAdapter);
+
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Country country = (Country) parent.getItemAtPosition(position);
+
+            }
+        });
+
+    }
+
+    private class MyCustomAdapter extends ArrayAdapter<Country> {
+
+        private ArrayList<Country> countryList;
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               ArrayList<Country> countryList) {
+            super(context, textViewResourceId, countryList);
+            this.countryList = new ArrayList<Country>();
+            this.countryList.addAll(countryList);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater)getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.list_item, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener( new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v ;
+                        Country country = (Country) cb.getTag();
+
+
+                    }
+                });
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Country country = countryList.get(position);
+            holder.code.setText(" (" +  country.getCode() + ")");
+            holder.name.setText(country.getName());
+            holder.name.setChecked(country.isSelected());
+            holder.name.setTag(country);
+
+            return convertView;
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                StringBuffer responseText = new StringBuffer();
+
+                ArrayList<Country> countryList = dataAdapter.countryList;
+                for(int i=0;i<countryList.size();i++){
+                    Country country = countryList.get(i);
+                    if(country.isSelected()){
+                        responseText.append(country.getName()+"$");
+                    }
+                }
+
+
+                Intent i = new Intent(MyActivity.this,Feeds.class);
+                i.putExtra("selected_categories",responseText.toString());
+                startActivity(i);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
